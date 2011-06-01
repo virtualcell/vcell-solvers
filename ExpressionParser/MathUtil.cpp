b5343921bb4efde2665291e38a56b44a449ac590
Fei Gao <fgao@uchc.edu>
2011-06-01 17:32:59 +0000

    Bug 2390  	remove references to flanagan math
    svn path=/vcell/trunk/numerics/; revision=8930
    
    
    Former-commit-id: 0d183a2c63ac0938efbe4527b3be4fa2ff99c9f2

--- ExpressionParser/MathUtil.cpp
+++ ExpressionParser/MathUtil.cpp
@@ -9,101 +9,79 @@ static double MathUtil::double_infinity = INFINITY;
 double MathUtil::double_infinity = std::numeric_limits<double>::infinity();
 #endif
 
-// Inverse hyperbolic cosine of a double number
-double MathUtil::acosh(double a){
-    return log(a+sqrt(a*a-1));
+double MathUtil::acosh(double arg){
+    return log(arg+sqrt(arg*arg-1));
 }
 
-// Inverse cotangent
-double MathUtil::acot(double a){
-    return atan(1.0/a);
+double MathUtil::acot(double arg){
+    return atan(1.0/arg);
 }
 
-double MathUtil::acot2(double a, double b){
-    return atan2(b, a);
+double MathUtil::acoth(double arg){
+    return 0.5*log((arg + 1.0)/(arg - 1.0));
 }
 
-// Inverse hyperbolic cotangent of a double number
-double MathUtil::acoth(double a){
-    return 0.5*log((a + 1.0)/(a - 1.0));
+double MathUtil::acsc(double arg){
+    return asin(1.0/arg);
 }
 
-// Inverse cosecant
-double MathUtil::acsc(double a){
-    return asin(1.0/a);
-}
-
-// Inverse hyperbolic cosecant of a double number
-double MathUtil::acsch(double a){
-	if (a > 0) {
-		return log( (1.0 + sqrt(1 + a*a)) / a );
-	} else if (a < 0) {
-		return log( (1.0 - sqrt(1 + a*a)) / a );
+double MathUtil::acsch(double arg){
+	if (arg > 0) {
+		return log( (1.0 + sqrt(1 + arg * arg)) / arg );
+	} else if (arg < 0) {
+		return log( (1.0 - sqrt(1 + arg * arg)) / arg );
 	}
 } 
 
-// Inverse secant
-double MathUtil::asec(double a){
-    return acos(1.0/a);
+double MathUtil::asec(double arg){
+    return acos(1.0/arg);
 }
 
-// Inverse hyperbolic secant of a double number
-double MathUtil::asech(double a){
-	return log( (1.0 + sqrt(1.0 - a*a)) / a );
+double MathUtil::asech(double arg){
+	return log( (1.0 + sqrt(1.0 - arg * arg)) / arg );
 }
 
-// Inverse hyperbolic sine of a double number
-double MathUtil::asinh(double a){
-    return log(a+sqrt(a*a+1));
+double MathUtil::asinh(double arg){
+    return log(arg + sqrt(arg * arg + 1));
 }
 
-// Inverse hyperbolic tangent of a double number
-double MathUtil::atanh(double a){
-    return 0.5*(log( (1.0 + a)/(1.0 - a) ));
+double MathUtil::atanh(double arg){
+    return 0.5*(log( (1.0 + arg)/(1.0 - arg) ));
 }
 
-// Cotangent
-double MathUtil::cot(double a){
-    return 1.0/tan(a);
+double MathUtil::cot(double arg){
+    return 1.0/tan(arg);
 }
 
-//Hyperbolic cotangent of a double number
-double MathUtil::coth(double a){
-    return 1.0/tanh(a);
+double MathUtil::coth(double arg){
+    return 1.0/tanh(arg);
 }
 
-// Cosecant
-double MathUtil::csc(double a){
-    return 1.0/sin(a);
+double MathUtil::csc(double arg){
+    return 1.0/sin(arg);
 }
 
-//Hyperbolic cosecant of a double number
-double MathUtil::csch(double a){
-    return 1.0/sinh(a);
+double MathUtil::csch(double arg){
+    return 1.0/sinh(arg);
 }
 
-// Secant
-double MathUtil::sec(double a){
-    return 1.0/cos(a);
-}
-
-//Hyperbolic secant of a double number
-double MathUtil::sech(double a){
-    return 1.0/cosh(a);
-}
-
-// factorial of n
-// Argument is of type double but must be, numerically, an integer
-// factorial returned as double but is, numerically, should be an integer
-// numerical rounding may makes this an approximation after n = 21
-double MathUtil::factorial(double n){
+double MathUtil::factorial(double arg){
     double f = 1.0;
-    int nn = (int)n;
-    for(int i = 1; i <= nn; i ++)
+    int n = (int)arg;
+	for(int i = 1; i <= n; i ++) {
 		f *= i;
+	}
     return f;
 }
 
-double MathUtil::round(double a) {
-	return floor(a+0.5);
+double MathUtil::round(double arg) {
+	return floor(arg + 0.5);
+}
+
+double MathUtil::sec(double arg){
+    return 1.0/cos(arg);
+}
+
+double MathUtil::sech(double arg){
+    return 1.0/cosh(arg);
 }
--- ExpressionParser/MathUtil.h
+++ ExpressionParser/MathUtil.h
@@ -3,133 +3,26 @@
 
 class MathUtil {
 public:
-    // Cotangent
-    static double cot(double a);
-
-    // Secant
-    static double sec(double a);
-
-    // Cosecant
-    static double csc(double a);
-
-    // Inverse functions of real numbers
-
-    // Inverse cotangent
-    static double acot(double a);
-
-    static double acot2(double a, double b);
-
-    // Inverse secant
-    static double asec(double a);
-
-    // Inverse cosecant
+	static double acosh(double a);
+    static double acot(double a); 
+	static double acoth(double a);
     static double acsc(double a);
-
-    //Hyperbolic cotangent of a double number
-    static double coth(double a);
-
-    //Hyperbolic secant of a double number
-    static double sech(double a);
-
-    //Hyperbolic cosecant of a double number
-    static double csch(double a);
-
-    // Inverse hyberbolic functions of real numbers
-
-    // Inverse hyperbolic sine of a double number
-    static double asinh(double a);
-
-    // Inverse hyperbolic cosine of a double number
-    static double acosh(double a);
-
-    // Inverse hyperbolic tangent of a double number
+	static double acsch(double a);	
+	static double asec(double a);
+    static double asech(double a);    
+    static double asinh(double a);    
     static double atanh(double a);
 
-    // Inverse hyperbolic cotangent of a double number
-    static double acoth(double a);
+	static double cot(double a);
+    static double coth(double a);	
+    static double csc(double a);
+    static double csch(double a);
 
-    // Inverse hyperbolic secant of a double number
-    static double asech(double a);
-
-    // Inverse hyperbolic cosecant of a double number
-    static double acsch(double a);  
-
-    // factorial of n
-    // argument and return are integer, therefore limited to 0<=n<=12
-    // see below for long and double arguments
-    static int factorial(int n);
-
-    // factorial of n
-    // argument and return are long, therefore limited to 0<=n<=20
-    // see below for double argument
-    static long factorial(long n);
-
-    // factorial of n
-    // Argument is of type double but must be, numerically, an integer
-    // factorial returned as double but is, numerically, should be an integer
-    // numerical rounding may makes this an approximation after n = 21
     static double factorial(double n);
-
-    // log to base e of the factorial of n
-    // log[e](factorial) returned as double
-    // numerical rounding may makes this an approximation
-    static double logFactorial(int n);
-
-    // log to base e of the factorial of n
-    // Argument is of type double but must be, numerically, an integer
-    // log[e](factorial) returned as double
-    // numerical rounding may makes this an approximation
-    static double logFactorial(double n);
-
-    // Returns the length of the hypotenuse of a and b
-    // i.e. sqrt(a*a+b*b) [without unecessary overflow or underflow]
-    // double version
-    static double hypot(double aa, double bb);
-
-    // Returns the length of the hypotenuse of a and b
-    // i.e. sqrt(a*a+b*b) [without unecessary overflow or underflow]
-    // float version
-    static float hypot(float aa, float bb);
-
-    // Maximum of a 1D array of doubles, aa
-    static double maximum(double* aa, int length);
-
-    // Maximum of a 1D array of floats, aa
-    static float maximum(float* aa, int length);
-
-    // Maximum of a 1D array of ints, aa
-    static int maximum(int* aa, int length);
-
-    // Minimum of a 1D array of doubles, aa
-    static double minimum(double* aa, int length);
-
-    // Minimum of a 1D array of floats, aa
-    static float minimum(float* aa, int length);
-
-    // Minimum of a 1D array of ints, aa
-    static int minimum(int* aa, int length);
-
-    // sort elements in an array of doubles into ascending order
-    // using selection sort method
-    static double* selectionSort(double* aa, int length); 
-
-    /*      returns -1 if x < 0 else returns 1   */
-    //  double version
-    static double sign(double x);
-
-    /*      returns -1 if x < 0 else returns 1   */
-    //  float version
-    static float sign(float x);
-
-    /*      returns -1 if x < 0 else returns 1   */
-    //  int version
-    static int sign(int x);
-
-    /*      returns -1 if x < 0 else returns 1   */
-    // long version
-    static long sign(long x);
-
 	static double round(double x);
+
+    static double sec(double a);
+	static double sech(double a);
 
 	static double double_infinity;
 };
