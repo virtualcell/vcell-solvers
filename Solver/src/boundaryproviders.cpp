44647fb31c7abc1735a5b3f3a7fdac73abe890e5
Gerard Weatherby <gweatherby@uchc.edu>
2014-01-31 22:26:57 +0000

    add Rectangle
    svn path=/vcell/branches/MovingBoundary/; revision=14583

--- Solver/src/boundaryproviders.cpp
+++ Solver/src/boundaryproviders.cpp
@@ -15,7 +15,7 @@ namespace {
 		moving_boundary::FrontType baseFront;
 		FixedBoundary(double xvelocity) 
 			:world(WorldType::get( )),
-			xVel(xvelocity),
+			xVel(xvelocity * world.theScale( ) ),
 			time(0) { }
 
 
@@ -37,7 +37,7 @@ namespace {
 		/**
 		* make self a transform operator
 		*/
-		moving_boundary::FrontPointType operator( )(moving_boundary::FrontPointType & in) {
+		moving_boundary::FrontPointType operator( )(const moving_boundary::FrontPointType & in) {
 			moving_boundary::FrontPointType rval(in);
 			rval(spatial::cX) += static_cast<moving_boundary::CoordinateType>(xVel *time + 0.5);
 			return rval;
@@ -99,15 +99,114 @@ namespace {
 		const double s; 
 	};
 
-	spatial::FrontProvider<moving_boundary::CoordinateType> *frontFromXMLCircle(const tinyxml2::XMLElement &node) {
-		double theta = vcell_xml::convertChildElement<double>(node,"thetaIncrement");
-		double vx = vcell_xml::convertChildElement<double>(node,"velocityx");
+	struct Rectangle : public FixedBoundary {
+		Rectangle(double originx, double originy, double width, double height, double velocityx)
+			:FixedBoundary(velocityx),
+			x(originx),
+			y(originy),
+			w(width),
+			h(height) 
+		{
+			spatial::Point2D cursor(originx,originy); 
+			using spatial::cX;
+			using spatial::cY;
+			baseFront.push_back(world.toWorld(cursor) );
+			cursor(cX) += w;
+			baseFront.push_back(world.toWorld(cursor) );
+			cursor(cX) += h;
+			baseFront.push_back(world.toWorld(cursor) );
+			cursor(cX) -= w;
+			baseFront.push_back(world.toWorld(cursor) );
+			close( );
+		}
 
-		double x = vcell_xml::convertChildElementWithDefault<double>(node,"originx",0);
-		double y = vcell_xml::convertChildElementWithDefault<double>(node,"originy",0);
-		double radius = vcell_xml::convertChildElementWithDefault<double>(node,"radius",1);
-		return new Circle(x,y,radius,theta,vx);
-	}
+		std::string describe( ) const {
+			std::ostringstream os;
+			os << "Rectangle corner at " << x << ',' << y << " width " << w << " height " << h 
+				<< "and x velocity " << xVel << std::endl;
+			return os.str( ); 
+			return std::string( );
+		}
+
+	private:
+		const double x; 
+		const double y; 
+		const double w;
+		const double h; 
+	};
+
+	/**
+	* base class / factory for alternate front providers
+	*/
+	struct Builder {
+		Builder(Builder *sub) {
+			builders[bIndex++] = sub;
+		}
+		virtual ~Builder( ) {}
+		/**
+		* return provider correct XML present
+		* @return nullptr if top level node doesn't match
+		*/
+		virtual spatial::FrontProvider<moving_boundary::CoordinateType> * build(const tinyxml2::XMLElement &node) = 0; 
+		static spatial::FrontProvider<moving_boundary::CoordinateType> * construct( const tinyxml2::XMLElement &node) {
+			for (size_t i = 0; i < builders.size( ) ; i++) {
+				Builder * b = builders[i];
+				if (b == nullptr) { //no match
+					return nullptr;
+				}
+				spatial::FrontProvider<moving_boundary::CoordinateType> * fp = b->build(node); 
+				if (fp != nullptr) {
+					return fp;
+				}
+			}
+			return nullptr;
+		}
+	protected:
+		static std::array<Builder*,10> builders;
+		static size_t bIndex;
+	};
+	std::array<Builder*,10> Builder::builders;
+	size_t Builder::bIndex;
+
+
+	struct CircleBuild : public Builder {
+		CircleBuild( )
+			:Builder(this) {}
+		virtual spatial::FrontProvider<moving_boundary::CoordinateType> * build(const tinyxml2::XMLElement &root) {
+			const tinyxml2::XMLElement *n = root.FirstChildElement("circle");
+			if (n != nullptr) {
+				const tinyxml2::XMLElement &node = *n; 
+				double theta = vcell_xml::convertChildElement<double>(node,"thetaIncrement");
+				double vx = vcell_xml::convertChildElement<double>(node,"velocityx");
+
+				double x = vcell_xml::convertChildElementWithDefault<double>(node,"originx",0);
+				double y = vcell_xml::convertChildElementWithDefault<double>(node,"originy",0);
+				double radius = vcell_xml::convertChildElementWithDefault<double>(node,"radius",1);
+				return new Circle(x,y,radius,theta,vx);
+			}
+			return nullptr;
+		}
+	} Cb;
+
+	struct RectangleBuild : public Builder {
+		RectangleBuild( )
+			:Builder(this) {}
+		virtual spatial::FrontProvider<moving_boundary::CoordinateType> * build(const tinyxml2::XMLElement &root) {
+			const tinyxml2::XMLElement *n = root.FirstChildElement("rectangle");
+			if (n != nullptr) {
+				const tinyxml2::XMLElement &node = *n; 
+				double width = vcell_xml::convertChildElement<double>(node,"width");
+				double height = vcell_xml::convertChildElement<double>(node,"height");
+				double vx = vcell_xml::convertChildElement<double>(node,"velocityx");
+
+				double x = vcell_xml::convertChildElementWithDefault<double>(node,"originx",0);
+				double y = vcell_xml::convertChildElementWithDefault<double>(node,"originy",0);
+				double radius = vcell_xml::convertChildElementWithDefault<double>(node,"radius",1);
+				return new Rectangle(x,y,width,height,vx); 
+			}
+			return nullptr;
+		}
+	} Rb;
 
 }
 
@@ -121,10 +220,12 @@ namespace {
 spatial::FrontProvider<moving_boundary::CoordinateType> * moving_boundary::circleFront(double originx, double originy, double radius, double step, double velocityx) {
 	return new Circle(originx,originy,radius,step,velocityx);
 }
+
+
 spatial::FrontProvider<moving_boundary::CoordinateType> * moving_boundary::frontFromXML(const tinyxml2::XMLElement &node) {
-	const tinyxml2::XMLElement *c = node.FirstChildElement("circle");
-	if (c != nullptr) {
-		return frontFromXMLCircle(*c);
+	spatial::FrontProvider<moving_boundary::CoordinateType> * fp = Builder::construct(node); 
+	if (fp != nullptr) {
+		return fp;
 	}
-	throw std::invalid_argument("unrecognized XML");
+	VCELL_EXCEPTION(invalid_argument,"Invalid XML for alternate front");
 }
