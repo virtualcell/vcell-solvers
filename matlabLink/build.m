ef9b46d093ad6ab0576067900639bf9eb0f6d0d2
Gerard Weatherby <gweatherby@uchc.edu>
2014-05-09 18:50:57 +0000

    add code to call clipper in manner expected by Matlab Production prototype (almost)
    svn path=/vcell/branches/MovingBoundary/; revision=15135

--- matlabLink/build.m
+++ matlabLink/build.m
@@ -3,3 +3,5 @@
 %mex -I../src -I../boost_1_53_0 -I../src/util -I../VCell/include -DWIN32  -L..\Debug -lFronTier -lVCellFronTier -lmsvcrtd -g -D_DEBUG mexFT_Step.cpp 
 fprintf( 'Building clipperlink\n')
 mex -f build.opts -I../src -I../src/util -DWIN32  -L..\bin -lExpressionParser -lfrontier -lmatlabLink -lMovingBoundaryLib  -g -D_DEBUG clipperLink.cpp
+fprintf( 'Building clipper\n')
+mex -f build.opts -I../src -I../src/util -DWIN32  -L..\bin -lExpressionParser -lfrontier -lmatlabLink -lMovingBoundaryLib  -g -D_DEBUG clipper.cpp
---
+++ matlabLink/matlabStruct.h
@@ -0,0 +1,52 @@
+#include <map>
+#include <string>
+#include <stdint.h>
+#include "mex.h"
+namespace matlabLink {
+	template <class T>
+	struct MData {
+		MData( )
+			:size(0),
+			data(0) {}
+		MData(size_t size_, T *array)
+			:size(size_),
+			data(array) {}
+		size_t size;
+		T *data;
+	};
+
+	struct MatlabStruct {
+		MatlabStruct(const mxArray *source);
+		MData<double> doubles(const char * const name, bool required=true) const;
+		std::string str(const char *const name, bool required = true) const;
+		/**
+		* fetch string, return default value if not present in structure
+		*/
+		std::string str(const char *const name, const char * const defaultValue) const;
+		bool boolean(const char *const name, bool required = true) const;
+		MData<int64_t> int64s(const char * const name, bool required=true) const;
+		struct ArrayData {
+			size_t size;
+			const mxArray *marray;
+			ArrayData( )
+				:size(0),
+				marray(0) {}
+		};
+	protected:
+		typedef bool (*validateFunction)(const mxArray *);
+
+		ArrayData get(const char * const name, bool required, const char *type, validateFunction) const; 
+
+		const mxArray &source;
+
+		typedef std::map<std::string,MData<double> > DoubleMap; 
+		mutable DoubleMap dblStore;
+
+		typedef std::map<std::string,std::string> StringMap; 
+		mutable StringMap strStore;
+
+		typedef std::map<std::string,MData<int64_t> > Int64Map; 
+		mutable Int64Map int64Store; 
+
+	};
+}
