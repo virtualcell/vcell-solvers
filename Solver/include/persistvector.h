c8c79dbbd38c0f42c46c61ecda6ebfda914120cd
Gerard Weatherby <gweatherby@uchc.edu>
2014-10-15 14:12:58 +0000

    gcc errors
    svn path=/vcell/branches/MovingBoundaryStateful/; revision=16204

--- Solver/include/persistvector.h
+++ Solver/include/persistvector.h
@@ -6,29 +6,6 @@
 #include <persist.h>
 namespace vcell_persist {
 	/**
-	* write vector of T which implements persist
-	*/
-	template<typename T>
-	void persistVector(std::ostream &os,  const std::vector<T> & vec) {
-		binaryWrite(os, vec.size( ));
-		std::for_each(vec.begin( ), vec.end( ),persistWrite<T>(os));
-	}
-
-	template<typename T>
-	void readVector(std::istream &is, std::vector<T> & vec) {
-		typedef std::vector<T>::size_type Stype;
-		Stype size; 
-		binaryRead(is,size);
-
-		vec.clear( );
-		vec.reserve(size);
-		for (Stype i = 0; i < size; ++i) {
-			T r(is);
-			vec.push_back( r );
-		}
-	}
-
-	/**
 	* std::for_each functor to persist data
 	*/
 	template<typename T>
@@ -41,5 +18,27 @@ namespace vcell_persist {
 		}
 	};
 
+	/**
+	* write vector of T which implements persist
+	*/
+	template<typename T>
+	void persistVector(std::ostream &os,  const std::vector<T> & vec) {
+		binaryWrite(os, vec.size( ));
+		std::for_each(vec.begin( ), vec.end( ),persistWrite<T>(os));
+	}
+
+	template<typename T>
+	void readVector(std::istream &is, std::vector<T> & vec) {
+		typedef typename std::vector<T>::size_type Stype;
+		Stype size; 
+		binaryRead(is,size);
+
+		vec.clear( );
+		vec.reserve(size);
+		for (Stype i = 0; i < size; ++i) {
+			T r(is);
+			vec.push_back( r );
+		}
+	}
 }	
 #endif
