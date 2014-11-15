d8f8a90b6c3bbdb7705ae8485a88055084b12483
Gerard Weatherby <gweatherby@uchc.edu>
2014-11-15 17:55:34 +0000

    add
    svn path=/vcell/branches/MovingBoundaryStateful/; revision=16374

---
+++ Solver/include/ReportClient.h
@@ -0,0 +1,23 @@
+#ifndef ReportClient_h
+#define ReportClient_h
+#include <MovingBoundarySetup.h>
+namespace tinyxml2 {
+	class XMLElement;
+}
+namespace moving_boundary {
+	class MovingBoundaryParabolicProblem;
+	struct ReportClient : public MovingBoundaryElementClient {
+		 virtual std::string getXML( ) const = 0;
+		 /**
+		 * @param root document root containing problem section
+		 * @param filename optional override of file name in XML, may be empty string
+		 * @param mbpp problem to report on
+		 */
+		 static ReportClient *setupReportClient(const tinyxml2::XMLElement &root, const std::string & filename, MovingBoundaryParabolicProblem &mbpp); 
+	};
+
+
+
+}
+
+#endif
