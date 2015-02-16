a106db5add1eaec039348357616ceee80038a99d
Gerard Weatherby <gweatherby@uchc.edu>
2015-02-16 14:42:41 +0000

    refactor
    svn path=/vcell/branches/MovingBoundary/; revision=16864

---
+++ matlab/clipperLink/svnversion.h
@@ -0,0 +1,10 @@
+#ifndef svnversion_h
+#define svnversion_h
+#if !defined(SVNVERSION)
+#error SVNVERSION version not defined
+#endif
+#define VCELLSVNQ(x) #x
+#define VCELLSVNQUOTE(x) VCELLSVNQ(x)
+//#define SVN_VERSION_TAG static const char * const svn_version_string = __FILE__"SVN_version:"VSTR(SVNVERSION) ;
+#define SVN_VERSION_TAG static const char * const svn_version_string = __FILE__" "__DATE__" SVN_version:  "VCELLSVNQUOTE(SVNVERSION); 
+#endif
