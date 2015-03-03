5ae902161b13285b46232d918ad77f036d31be2e
Gerard Weatherby <gweatherby@uchc.edu>
2015-03-03 16:21:02 +0000

    checkpoint prior to refactor
    svn path=/vcell/branches/MovingBoundary/; revision=16956

---
+++ Solver/src/Species.cpp
@@ -0,0 +1,18 @@
+#include <Species.h>
+#include <VCellException.h>
+using moving_boundary::biology::Species;
+
+void Species::setTable(const SimpleSymbolTable &symTable) {
+	sourceExp.bindExpression(symTable);
+	diffusionExp.bindExpression(symTable);
+	size_t n = diffusionExp.numberSymbols( );
+	actDiffSpecI.reserve(n);
+	std::vector<std::string> symbols = diffusionExp.getSymbols( );
+	for (std::string sym : symbols) {
+		SymbolTableEntry * ste = symTable.getEntry(sym);
+		if (ste == nullptr) {
+			VCELL_EXCEPTION(domain_error,"input sym table does not contain required symbol " << sym);
+		}
+		actDiffSpecI.push_back(ste->getIndex( ));
+	}
+}
