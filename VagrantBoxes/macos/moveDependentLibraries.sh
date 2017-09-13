84061287973dbf3a68a56fb96666f798208563b7
Jim Schaff <schaff@uchc.edu>
2017-09-13 18:08:20 -0400

    added test case for MovingBoundary, added missing mac libs.

--- VagrantBoxes/macos/moveDependentLibraries.sh
+++ VagrantBoxes/macos/moveDependentLibraries.sh
@@ -7,19 +7,12 @@ cd /vagrant_numerics/${builddir}/bin
 
 cp /usr/local/opt/hdf5@1.8/lib/libhdf5_cpp.14.dylib .
 cp /usr/local/opt/hdf5@1.8/lib/libhdf5.10.dylib .
+cp /usr/local/opt/hdf5@1.8/lib/libhdf5_hl_cpp.11.dylib .
+cp /usr/local/opt/hdf5@1.8/lib/libhdf5_hl.10.dylib .
 cp /usr/local/opt/szip/lib/libsz.2.dylib .
 cp /usr/local/lib/libgfortran.4.dylib .
 cp /usr/local/lib/libgcc_s.1.dylib .
 cp /usr/local/lib/libquadmath.0.dylib .
-
-#cp /usr/local/Cellar/hdf5@1.8/1.8.19/lib/libhdf5_cpp.14.dylib .
-#cp /usr/local/Cellar/hdf5@1.8/1.8.19/lib/libhdf5.10.dylib .
-#cp /usr/local/Cellar/hdf5@1.8/1.8.19/lib/libhdf5_hl_cpp.11.dylib .
-#cp /usr/local/Cellar/hdf5@1.8/1.8.19/lib/libhdf5_hl.10.dylib .
-#cp /usr/local/lib/libgfortran.4.dylib .
-#cp /usr/local/lib/libquadmath.0.dylib .
-#cp /usr/local/opt/szip/lib/libsz.2.dylib .
-#cp /usr/local/lib/libgcc_s.1.dylib .
 
 cd /vagrant_numerics/${builddir}/bin
 chmod +w *
