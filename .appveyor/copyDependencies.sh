ca33ea90037172d161d0982c480bea1a7ff369bd
Jim Schaff <schaff@uchc.edu>
2017-10-30 21:02:19 -0400

    appveyor: move bash ldd invocations to bash script.

---
+++ .appveyor/copyDependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+export PATH=/mingw64/bin:/usr/bin/:$PATH
+cd /c/projects/vcell-solvers/build/bin
+ldd FiniteVolume_x64.exe
+ldd NFsim_x64.exe
+ldd smoldyn_x64.exe
+ldd SundialsSolverStandalone_x64.exe
+ldd testzip.exe
+ldd VCellStoch_x64.exe
+ldd ziptool.exe
+ls *.exe | awk '{print $1}' | xargs -I '{}' ldd '{}' | grep '=> /' | grep -v build | grep -v Windows | awk '{print $3}' | xargs -I '{}' cp -vn '{}' . || true
+ls *.dll | awk '{print $1}' | xargs -I '{}' ldd '{}' | grep '=> /' | grep -v build | grep -v Windows | awk '{print $3}' | xargs -I '{}' cp -vn '{}' . || true
+ls *.dll | awk '{print $1}' | xargs -I '{}' ldd '{}' | grep '=> /' | grep -v build | grep -v Windows | awk '{print $3}' | xargs -I '{}' cp -vn '{}' . || true
