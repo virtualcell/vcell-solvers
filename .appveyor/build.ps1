56db9756e5b119906e50dc39c90f956c72771144
Mike Wilson <mpw6@users.noreply.github.com>
2018-10-01 15:08:26 -0400

    revert again

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -3,8 +3,8 @@ mkdir build
 cd build
 mkdir bin
 Write-Host "calling cmake in " + $pwd
-#There appears to be a problem with finding cmake after the MinGW installation of it.
-cmake -Wdev --trace-expand --debug-output -G "Unix Makefiles" `
+#There appears to be a problem with cmake creating the Makefile
+cmake -G "Unix Makefiles" `
     -DCMAKE_BUILD_TYPE="Release" `
     -DOPTION_TARGET_MESSAGING=OFF `
     -DOPTION_TARGET_PARALLEL=OFF `
