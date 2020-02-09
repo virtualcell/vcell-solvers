d899a0c634e316b11b7ed4c5e904a6688c3ad799
GMarupilla <53095348+gmarupilla@users.noreply.github.com>
2020-02-08 23:43:04 -0500

    Update build.ps1

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -9,20 +9,13 @@ cmake -G "Unix Makefiles" `
     -DOPTION_TARGET_PARALLEL=OFF `
     -DOPTION_TARGET_CHOMBO2D_SOLVER=OFF `
     -DOPTION_TARGET_CHOMBO3D_SOLVER=OFF `
-    -DOPTION_TARGET_SMOLDYN_SOLVER=ON `
-    -DOPTION_TARGET_FV_SOLVER=ON `
-    -DOPTION_TARGET_STOCHASTIC_SOLVER=ON `
+    -DOPTION_TARGET_SMOLDYN_SOLVER=OFF `
+    -DOPTION_TARGET_FV_SOLVER=OFF `
+    -DOPTION_TARGET_STOCHASTIC_SOLVER=OFF `
     -DOPTION_TARGET_NFSIM_SOLVER=ON `
     -DOPTION_TARGET_MOVINGBOUNDARY_SOLVER=OFF `
-    -DOPTION_TARGET_SUNDIALS_SOLVER=ON `
+    -DOPTION_TARGET_SUNDIALS_SOLVER=OFF `
     -DOPTION_TARGET_HY3S_SOLVERS=OFF `
      ..
 make
 
-ldd .\build\bin\FiniteVolume_x64.exe
-ldd .\build\bin\NFsim_x64.exe
-ldd .\build\bin\smoldyn_x64.exe
-ldd .\build\bin\SundialsSolverStandalone_x64.exe
-ldd .\build\bin\testzip.exe
-ldd .\build\bin\VCellStoch_x64.exe
-ldd .\build\bin\ziptool.exe
