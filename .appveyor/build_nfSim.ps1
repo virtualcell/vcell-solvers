f6e9f587b9849a5e7efcded0676e4ffb79aa7414
gmarupilla <marupilla@uchc.edu>
2020-03-03 10:50:58 -0500

    Test by building all solvers seperatley from different scripts

---
+++ .appveyor/build_nfSim.ps1
@@ -0,0 +1,25 @@
+$Env:Path = "c:\msys64\mingw64\bin" + ";" + "c:\msys64\usr\bin" + ";" + $Env:Path
+mkdir tmp
+mkdir build
+cd build
+mkdir bin
+Write-Host "calling cmake in " + $pwd
+
+cmake -G "Unix Makefiles" `
+    -DBUILD_SHARED_LIBS=OFF `
+    -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" `
+    -DCMAKE_EXE_LINKER_FLAGS="-static" `
+    -DOPTION_TARGET_MESSAGING=OFF `
+    -DOPTION_TARGET_PARALLEL=OFF `
+    -DOPTION_TARGET_CHOMBO2D_SOLVER=OFF `
+    -DOPTION_TARGET_CHOMBO3D_SOLVER=OFF `
+    -DOPTION_TARGET_SMOLDYN_SOLVER=OFF `
+    -DOPTION_TARGET_FV_SOLVER=OFF `
+    -DOPTION_TARGET_STOCHASTIC_SOLVER=OFF `
+    -DOPTION_TARGET_NFSIM_SOLVER=ON `
+    -DOPTION_TARGET_MOVINGBOUNDARY_SOLVER=OFF `
+    -DOPTION_TARGET_SUNDIALS_SOLVER=OFF `
+    -DOPTION_TARGET_HY3S_SOLVERS=OFF `
+     ..
+
+make
