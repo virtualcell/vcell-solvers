f4691ef98ff7c23cdef6e91db2ae45e8bb811f4b
Jim Schaff <schaff.jim@gmail.com>
2023-08-08 18:07:24 -0400

    some fixes to FronTierLib, but don't build on macos for now

--- FronTierLib/front/fscat3d2.c
+++ FronTierLib/front/fscat3d2.c
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #define DEBUG_STRING    "fscatter"
 #include <front/fdecs.h>
-#include <intfc/ilocprotos.h>
+#include <intfc/idiagnostic.h>
 
 	/* LOCAL Function Declarations */
 LOCAL	INTERFACE *cut_buf_interface2(INTERFACE*,int,int,int*,int*);
