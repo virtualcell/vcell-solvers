23933cbe4703289a351409d7c004657df4dd94b9
Jim Schaff <schaff.jim@gmail.com>
2023-08-11 12:22:03 -0400

    CMake updates VCELL/GitDescribe.h for executable versioning

---
+++ VCellMessaging/include/VCELL/GitDescribe.h
@@ -0,0 +1,10 @@
+//
+// Created by Jim Schaff on 8/11/23.
+//
+
+#ifndef NUMERICS_GITDESCRIBE_H
+#define NUMERICS_GITDESCRIBE_H
+
+extern const char g_GIT_DESCRIBE[];
+
+#endif //NUMERICS_GITDESCRIBE_H
