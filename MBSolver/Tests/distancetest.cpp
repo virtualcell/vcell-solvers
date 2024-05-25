d0e965c9d8fb1abcf2b5d6b33d895d77a3b7bb8b
jcschaff <schaff.jim@gmail.com>
2024-05-25 09:24:44 -0400

    ARM has only 64bit long double, fixed a moving boundary test

--- MBSolver/Tests/distancetest.cpp
+++ MBSolver/Tests/distancetest.cpp
@@ -8,8 +8,10 @@ TEST(distance,basic) {
 	int64_t biggest = std::numeric_limits<int64_t>::max( );
 	int64_t okay = DefaultDistancePolicy<int64_t>::convert(biggest);
 	ASSERT_TRUE(okay == biggest);
-    // jcs: switched test to long double, a double cannot hold int64_t max+1 without underflow, using long double
+    // jcs: switched test to long double, a double cannot hold int64_t max+1 without overflow, using long double
 	long double tooBig = static_cast<long double>(biggest) + 1;
+#ifndef __arm64__  // jcs: arm64 does has only 64-bit long double, so this test will fail
 	ASSERT_THROW(DefaultDistancePolicy<int64_t>::convert(tooBig),std::domain_error);
+#endif
 	cout << okay << endl;
 }
